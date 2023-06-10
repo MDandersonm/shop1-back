5b2f16ab71fb96425560026c2ad06533e8e47e5d
MDandersonm <1215kimyj@gmail.com>
2023-06-10 23:26:18 +0900

    Initial commit

---
+++ src/main/java/com/example/shop1back/Shop1BackApplication.java
@@ -0,0 +1,13 @@
+package com.example.shop1back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Shop1BackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Shop1BackApplication.class, args);
+	}
+
+}
